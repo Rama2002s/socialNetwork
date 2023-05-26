8d2e0da69039c61db445f0979d5aaf3852ec883c
Rama2002s <rama2002sriram@gmail.com>
2023-05-27 00:10:59 +0530

    Messaging system completed.
    Source code for 11 - 10.1 is wrong, So needed to redo the entire thing.

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 require 'config/config.php';
 include("includes/classes/User.php");
 include("includes/classes/Post.php");
+include("includes/classes/Message.php");
 
 if (isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
@@ -40,21 +41,93 @@ if (isset($_SESSION['username'])) {
             <a href="index.php">The Social Net</a>
         </div>
         <nav>
+        <?php
+				//Unread messages 
+				$messages = new Message($con, $userLoggedIn);
+				$num_messages = $messages->getUnreadNumber();
+			?>
             <a href="<?php echo $userLoggedIn; ?>">
                 <?php
                 echo $user['first_name'];
                 ?>
             </a>
-            <a href="#"><i id="msg" class="fa-solid fa-message fa-lg  "> </i></a>
-            <a href="index.php"><i class="fa-solid fa-house fa-lg  "> </i></a>
-            <a href="#"><i class="fa-solid fa-bell fa-lg  "></i></a>
-            <a href="#"><i class="fa-solid fa-users fa-lg  "></i></a>
-            <a href="#"><i class="fa-solid fa-gear fa-lg"></i></a>
+            <a href="index.php">
+                <i class="fa-solid fa-house fa-lg  "> </i>
+            </a>
+            <a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
+            <i id="msg" class="fa-solid fa-message fa-lg  "> </i>
+            <?php
+				if($num_messages > 0)
+				 echo '<span class="notification_badge" id="unread_message">' . $num_messages . '</span>';
+				?>
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-bell fa-lg  "></i>
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-users fa-lg  "></i>
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-gear fa-lg"></i>
+            </a>
             <a href="includes/handlers/logout.php">
                 <i class="fa fa-sign-out fa-lg"></i>
             </a>
 
         </nav>
+
+        <div class="dropdown_data_window" style="height:0px; border:none;"></div>
+        <input type="hidden" id="dropdown_data_type" value="">
+        
     </div>
+
+    <script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('.dropdown_data_window').scroll(function() {
+			var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+			var scroll_top = $('.dropdown_data_window').scrollTop();
+			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+
+			if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+
+				var pageName; //Holds name of page to send ajax request to
+				var type = $('#dropdown_data_type').val();
+
+
+				if(type == 'notification')
+					pageName = "ajax_load_notifications.php";
+				else if(type = 'message')
+					pageName = "ajax_load_messages.php"
+
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/" + pageName,
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache:false,
+
+					success: function(response) {
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
+
+
+						$('.dropdown_data_window').append(response);
+					}
+				});
+
+			} //End if 
+
+			return false;
+
+		}); //End (window).scroll(function())
+
+
+	});
+
+	</script>
 
     <div class="wrapper">
