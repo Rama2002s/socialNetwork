862b5f7808a78d23385423a4700a9a9f27df0cae
Rama Sriram <rama2002sriram@gmail.com>
2023-06-02 15:44:27 +0530

    Message.php still not working as intended.

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -201,14 +201,20 @@ class Message {
 			if($num_iterations++ < $start)
 				continue;
 
-			if($count++ > $limit)
+			if($count > $limit)
 				break;
-			
+			else 
+				$count++;
 
 
 			$is_unread_query = mysqli_query($this->con, "SELECT opened FROM messages WHERE user_to='$userLoggedIn' AND user_from='$username' ORDER BY id DESC");
 			$row = mysqli_fetch_array($is_unread_query);
-			$style = ($row['opened'] == 'no') ? "background-color: #DDEDFF;" : "";
+			if($row['opened'] == 'no')
+				$style =  "background-color: #DDEDFF;";
+			else
+				$style = "";
+				
+			// $style = ($row['opened'] == 'no') ? "background-color: #DDEDFF;" : "";
 
 
 			$user_found_obj = new User($this->con, $username);
